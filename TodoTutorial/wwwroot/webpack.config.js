1f51ee392dbeeb6a6721f92e636d77f814ea303f
Глагольев Игорь <defazze@gmail.com>
2018-08-14 07:44:16 +0300

    Webpack added

---
+++ TodoTutorial/wwwroot/webpack.config.js
@@ -0,0 +1,12 @@
+var path = require("path");
+var webpack = require("webpack");
+
+module.exports = {
+  devtool: "eval",
+  mode: "development",
+  entry: ["./src/index"],
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devServer: {
+    port: 3000
+  }
+};
